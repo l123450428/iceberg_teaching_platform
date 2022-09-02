19357738a3c227eb77321ad1576dcd93d5573e18
l123450428 <l123450428@gmail.com>
2022-09-02 15:49:05 +0800

    外語破冰EDM活動頁面

---
+++ iceberg_teaching_platform/index.php
@@ -0,0 +1,468 @@
+<?php
+$NOW_EDM='外語破冰計劃';
+require_once("$_SERVER[DOCUMENT_ROOT]/includes/cookie-set.php");?>
+<!DOCTYPE html>
+
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>外語破冰計劃｜HiTutor線上英文、線上外語家教</title>
+
+<meta name="keywords" content="免費學英文,線上學英文,學英語,外語,生活英文,實用英文,商業英文,日文,韓文,法文,德文,西文,歐語,泰文,印尼文,越南文,菲律賓語,英文翻譯,中翻英,線上翻譯,免費翻譯,hitutor,HiTutor,線上外語家教,在家學外語,線上英文推薦,教學平台,破冰計劃,冰山計劃,SQ3R, 免費體驗">
+<meta name="description" content="「學習不是注滿一桶水，而是點燃一把火」。你學英文的方法，是時候該改變了！預習、上課、複習，一站就學成。上課平台免安裝，結合SQ3R學習法，線上老師幫助你燃燒對新語言的熱情！生活會話、旅遊外語、考試檢定都該優先選用的破冰計劃。">
+
+<meta property="og:title" content="外語破冰計劃｜HiTutor線上英文、線上外語家教" >
+<meta property="og:url" content="http://www.hitutor.com.tw/online-learing-english/iceberg_teaching_platform/index.php">
+<meta property="og:image" content="http://www.hitutor.com.tw/online-learing-english/iceberg_teaching_platform/images/shar.png">
+<meta property="og:description" content="「學習不是注滿一桶水，而是點燃一把火」。你學英文的方法，是時候該改變了！預習、上課、複習，一站就學成。上課平台免安裝，結合SQ3R學習法，線上老師幫助你燃燒對新語言的熱情！生活會話、旅遊外語、考試檢定都該優先選用的破冰計劃。" >
+
+<!-- Style已包含: Bootstrap v4.0.0-beta.3 + fontawesome-free-5.0.8 -->
+
+
+<?php include_once('../form_basic_rwd/edm_form_include/edm_css.php');?>
+
+<!-- ↓ 此區載入設計師的 CSS ↓ -->
+  
+      <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+<link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css'>
+   <link href="css/iconstyle.css" rel="stylesheet">
+    <link href="css/resume.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+  <!-- ↑ 此區載入設計師的 CSS ↑ -->
+<?php include_once("../../yahoo/analyticstracking.php") ?>
+
+</head>
+
+  <body >
+	 
+
+		  <div class="freeclass d-none d-xl-block " id="freec">	 <a class="js-scroll-trigger" href="#amy_upset">
+			  <img  src="images/freeclass.png" ></a>
+	  
+	  
+	  </div> 
+	  
+	  
+<!--導覽列開始-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav"> 
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class="img-fluid d-block d-lg-none" src="images/logo.png" >
+        <span class="d-none d-lg-block "></span>
+       
+      </a>
+		<!--手機版按鈕-->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+		<!--手機版按鈕結束-->
+		 <div class="hitutorlogo d-none d-lg-block"><a href="http://tw.hitutoracdm.com/" target="_blank" onMouseOver="MM_swapImage('Image24','','images/logo-1.png',1)" onMouseOut="MM_swapImgRestore()"><img src="images/logo.png" alt=""  id="Image24"></a></div>
+		
+		
+		
+      <div class="collapse navbar-collapse " id="navbarSupportedContent">
+	  <div id="top-bar" ><a class="nav-link js-scroll-trigger  d-none d-lg-block" href="#page"><img src="images/gotop.png" ></a></div>
+<ul class="navbar-nav">
+      <li class="nav-item">
+            <a class="nav-link js-scroll-trigger " href="#amybgr" ><font class="amyvertical-lr">Amy</font><font class="foramy">的煩惱</font></a>
+      </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#iceberg" >冰山計劃</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#mother_like">你阿嬤都愛的</a>
+          </li>
+	
+      <p style="color: #FFFFFF " class="d-none d-lg-block">SHARE</p>
+
+         <a href="https://www.facebook.com/hitutor.esl/" target="_blank" class="icon-button facebook" title="HiTutor Facebook"><i class="icon-facebook"></i><span></span></a>
+		<a href="https://www.youtube.com/channel/UCiJj_8srFv1yWwh_hQ5B3sg" target="_blank" class="icon-button youtube" title="HiTutor Youtube"><i class="fa fa-youtube"></i><span></span></a>
+</ul>
+		 		
+      </div>
+		
+
+	
+		
+		
+		
+  </nav>  
+<!--導覽列結束-->
+      
+<!--首頁-->
+      <section class="resume-section" id="page">
+	    <div class="newsystem"><img class="img-fluid d-none d-lg-block" src="images/new_ststem_icon.png" ></div>
+<div class="sec1-col">
+          <div class="container-fluid" >
+			   <div class="row">
+				   <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+				     <div class="pagetitle"><center><img class="img-fluid d-none d-md-block" src="images/pagetitle.png" ></center>
+						 <img class="img-fluid d-block d-md-none" src="images/pagetitle-mobile.png" >
+					   <p>學習系統新上線，帶你<font style="color: #6EC3B3">快狠準學好語言！</font></p>
+				     </div>
+                 </div> 
+				    <div class="col-md-12 ">
+				     <div class="pagetitle2">
+					   <img class="img-fluid" src="images/pagetitle-2.png" > </div>
+                 </div> 
+				   
+				   
+				<!--   手機板-->
+				   <div class="row no-gutters" id="mobilepage">
+				     <div class="col-md-12">
+				  
+					   <img class="img-fluid d-block d-md-none" src="images/mobilephoto.png" > 
+                 </div> </div>
+				   <!--   手機板-->
+				   
+				   
+				   
+				   
+		    </div></div>
+		  <div id="amy_upset"></div>
+	<div class="but" >
+		   <div class="container-fluid " style="margin: 0; padding: 0;" >
+			   <div class="row no-gutters" >
+				   
+				   
+				   
+			
+				
+				
+				
+				
+				    <div class="col-lg-6 col-md-12 " >
+				  	     <div class="thumb effect-1">
+    <img class="imgfull d-none d-md-block" src="images/pagephoto.png" />
+
+    <div class="retina2">
+    
+
+      <p>"<br>
+
+  &nbsp;&nbsp;&nbsp;&nbsp;老師引導你<br>
+
+		  <font style="color: #000000">&nbsp;&nbsp;&nbsp;&nbsp;被動</font>接收老師<br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;給你的資訊<br>
+
+ &nbsp;&nbsp;&nbsp;&nbsp;但能吸收的卻很有限<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		
+		
+		</p>
+    </div>
+  </div></div>
+		
+           
+			       <div class="col-lg-6 col-md-12 ">
+					  <div class="thumb effect-1">
+    <img class="imgfull d-none d-md-block" src="images/pagephoto-2.png" />
+
+    <div class="retina">
+    
+
+      <p>"<br>
+
+  &nbsp;&nbsp;&nbsp;&nbsp;老師引導你<br>
+
+		  <font style="color: #FFCC00">&nbsp;&nbsp;&nbsp;&nbsp;主動</font>燃起學習的熱情<br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;教你方法<br>
+
+ <font style="color: #FFCC00">&nbsp;&nbsp;&nbsp;&nbsp;挖掘整個知識的大冰山</font><br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		
+		
+		</p>
+    </div>
+  </div></div></div>
+             
+			  
+			   
+			   
+
+		  
+		  </div>
+		  
+    </div> 
+		  </div>
+</section>
+<!--首頁-->
+			
+<!--AMY的煩惱-->
+      <section class="resume-section" id="amybgr">
+		  
+<!--表單-->
+<div class="iframe">
+							<div class="container">
+
+<?php include_once('../form_basic_rwd/edm_form_include/edm_form.php');?></div>
+</div>	  
+ <!--表單結束--> 
+ <div class="showamy"> 
+			  <div class="bgdot">
+        <div class="container-fluid">
+	   
+		  
+		  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			  
+  <div class="carousel-inner ">
+    <div class="carousel-item active">
+     <img class="img-fluid d-none d-md-block" src="images/amy-1.png" alt="Second slide">
+		  <img class="img-fluid d-block d-md-none" src="images/mobile2.png" alt="Second slide" > 
+    </div>
+    <div class="carousel-item">
+      <img class="img-fluid d-none d-md-block" src="images/amy-2.png" alt="Second slide">
+		  <img class="img-fluid d-block d-md-none" src="images/amy-2-2.png" alt="Second slide" > 
+    </div>
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+   <img class="img-fluid" src="images/btn2.png" >
+   
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+<img class="img-fluid" src="images/btn.png" >
+  
+  </a>
+</div>
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  </div></div></div>
+      </section>
+<!--AMY的煩惱-->
+		
+<!--冰山計畫-->
+      <section class="resume-section" id="iceberg">
+	    <div class="sec3-col">
+     <div class="container" >
+			   <div class="row"> 
+				   <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-md-12">
+				     <div class="sec3-amy">
+						 <div class="amyim">  <img class="img-fluid" src="images/amy-3.png" ></div><div class="sec3-font"><p>Amy：<br>
+
+						 學英文想更有效，重點就是 <font style="color: #6EC3B3">SQ3R</font><br><div class="line"></div><p>
+
+讓我 <font style="color: #6EC3B3">預習、複習、不斷練習</font></p> <div class="line"></div></div></div>
+                   </div>
+	    </div></div>
+		  <div class="sec3-ice-bg"> 
+			<div class="slogan"><img class="img-fluid" src="images/slogan.png" ></div>
+   <div class="container" >
+			   <div class="row"> 
+			  <div class="col-lg-4 col-md-6 col-sm-6">
+								 <div class="secim">      <img src="images/s.png" >  </div>
+				    <div class="SQ3R-title"><h3> <font style="color: #244588">S</font>urvey </h3>
+
+<p>概 覽</p>  </div>
+			    <p class="sq3rfont">上課前先預習大略的<br>
+
+把不懂的地方記下來	</p>
+				   
+				   
+			     </div>
+			     <div class="col-lg-4 col-md-6 col-sm-6">
+			  					 <div class="secim">    <img src="images/Q.png" > </div>
+				       <div class="SQ3R-title"><h3><font style="color: #244588">Q</font>uestion </h3>
+
+<p>提 問</p>  </div>
+				   <p class="sq3rfont">上課時，老師會引導<br>
+
+學生思考、解決問題</p>
+				   
+				   
+			     </div>
+				   
+				   
+				   
+				   
+				   
+				
+			     <div class="col-lg-4 col-md-12" id="trstyle">
+					 <div class="secim">  <img src="images/3R.png" > </div>
+	   
+	       <div class="SQ3R-title"><h4 > <font style="color: #244588">R</font>ead&nbsp; <font style="color: #244588">R</font>ecite&nbsp;
+
+ <font style="color: #244588">R</font>eview
+ </h4>
+
+<p>複 習&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;精 讀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;背 誦</p>  
+</div>
+				   <p class="sq3rfont">課後可隨時登入複習、練習
+<br>
+
+直到內化成自己理解的內容	</p>
+				   
+				   
+			     </div>
+	   
+	   </div>   </div>
+          </div>  
+	    </div>
+          </div>
+      </section>
+<!--冰山計畫-->
+ 
+<!--你阿嬤都愛的-->
+      <section class="resume-section" id="mother_like">
+     
+		 
+		  <div class="sec4-like">
+	 <div class="container">
+			   <div class="row">  
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
+				<div class="secimg"><img class="img-fluid" src="images/01.png" ></div>
+				   <p>預習、上課、複習<br>
+
+一站學成！</p>
+				   
+				   
+			     </div>
+				    <div class="col-lg-4 col-md-6 col-sm-6  col-xs-6">
+						<div class="secimg">  <img class="img-fluid" src="images/02.png" ></div>
+						
+						 <p>上課系統免安裝<br>
+
+隨時隨地學17國外語</p>
+				 </div>    <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+				   <div class="secimg">  <img class="img-fluid" src="images/03.png" ></div>
+				   
+				    <p>該告訴親朋好友的<br>
+
+						聰明學習法</p>
+        </div></div></div>  </div>
+		    <div class="sec4-like-2">
+		    <div class="col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-xs-6 offset-xs-3" id="btnbg">
+			  <p>—簡單到<font style="color:#FFB401">連你阿嬤都會用!</font>—</p>
+				 <a class="nav-link js-scroll-trigger" href="#amy_upset">
+				<div class="btn-primary" >免費試用體驗課</div></a>
+		  </div>
+		  
+		<div class="sec4-bgicon"><img src="images/sec4-bgicon.png" ></div></div>
+  </section>
+<!--你阿嬤都愛的-->
+ 
+ <!--Footer--> 
+  <footer class="edm_footer_001">
+<div class="container">
+<?php include_once('../form_basic_rwd/edm_form_include/footer.php');?>
+</div>
+</footer>
+ <!--Footer-->
+	
+	
+  <!-- ↓ Script已包含: jQuery v1.12.4 + Bootstrap v4.0.0-beta.3 ↓ -->
+<?php include_once('../form_basic_rwd/edm_form_include/edm_form_script.php'); ?>
+	  
+	  
+	  
+	  
+  <!-- ↓ 此區載入設計師的 Javascript ↓ -->
+
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+	
+    <script src="js/resume.min.js"></script>
+	   <script src="js/index.js"></script>
+	
+<script>
+$(function(){
+	$(window).load(function(){
+		$(window).bind('scroll resize', function(){
+			var $this = $(this);
+			var $this_Top=$this.scrollTop();
+			
+			//當高度小於100時，關閉區塊	
+			if($this_Top < 500){
+				$('#top-bar').fadeOut()};
+			
+	
+			//當高度小於100時，關閉區塊	
+			if($this_Top > 500){
+				$('#top-bar').fadeIn();
+			
+			
+			
+			
+}
+		}).scroll();
+	});
+});
+</script>  
+	<script>
+$(function(){
+	$(window).load(function(){
+		$(window).bind('scroll resize', function(){
+			var $this = $(this);
+			var $this_Top=$this.scrollTop();
+			
+			//當高度小於100時，關閉區塊	
+			if($this_Top < 300){
+				$('#freec img').animate({
+   
+   
+ 
+    width:'273px' },50)};
+			
+	
+			//當高度小於100時，關閉區塊	
+			if($this_Top > 300){
+				$('#freec img').animate({
+    
+   
+   
+    width:'200px'},50)};
+			
+			
+			
+			
+
+		}).scroll();
+	});
+});
+</script>  
+
+	
+	
+	
+  <script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+<!-- ↑ 此區載入設計師的 Javascript ↑ -->
+	  
+	  
+	  
+ <?php include_once("../../ga/analyticstracking.php") ?>
+<?php include_once("../../adwords/adwords.php") ?>
+</body>
+
+</html>
